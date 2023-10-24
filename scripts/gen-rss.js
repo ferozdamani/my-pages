8b79f21c23e8f843ca3bc4c18972dbd72ad83578
Feroz Hussain Damani <ferozhussaindamani@Ferozs-MacBook-Air.local>
2023-10-23 17:42:59 -0700

    chore: first page template cleanup

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Feroz Damani',
+    site_url: 'https://ferozdamani.tech',
+    feed_url: 'https://ferozdamani.tech/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
